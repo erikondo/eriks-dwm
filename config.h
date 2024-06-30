0d15059e4bac9464c2c4198f466004dafe02e570
Erik VanBaalen <erikvanbaalen@gmail.com>
2024-06-30 17:55:42 -0500

    MOVESTACK CHANGE

--- config.h
+++ config.h
@@ -78,6 +78,7 @@ static const char *mailcmd[]  = { "thunderbird", NULL };
 
 #include "selfrestart.c"
 #include "shiftview.c"
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -95,6 +96,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ALTMODKEY,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ALTMODKEY,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
