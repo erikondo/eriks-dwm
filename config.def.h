6c7e0b506c878adb149135c68008526cef74fc4a
Erik VanBaalen <erikvanbaalen@gmail.com>
2026-08-09 16:07:00 -0500

    added footswitch commands, may revert

--- config.def.h
+++ config.def.h
@@ -81,6 +81,7 @@ static const char *webcmd[]  = { "brave", NULL };
 static const char *sshotcmd[]  = { "screenshot", NULL };
 static const char *cocrcmd[]  = { "cocr", NULL };
 static const char *mailcmd[]  = { "thunderbird", NULL };
+static const char *mpxtoggle[] = { "/home/erik/.local/bin/mpx-toggle.sh", NULL };
 
 //Patch Includes
 #include "selfrestart.c"
@@ -128,6 +129,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
   // END
+  { MODKEY|ShiftMask,             XK_m,      spawn,          {.v = mpxtoggle } },
 	{ MODKEY,	                      XK_n, 	   shiftview,      {.i = +1 } },
 	{ MODKEY,	                      XK_b, 	   shiftview,      {.i = -1 } },
 	// { ALTMODKEY,                    XK_s,      show,           {0} },
